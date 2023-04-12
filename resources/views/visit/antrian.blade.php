60a610e3f63ff734a197f4e8b1e80679074da4bc
Muhammad <sunamuhardi@outlook.com>
2023-04-13 01:50:48 +0700

    odd even biar cakep

--- resources/views/visit/antrian.blade.php
+++ resources/views/visit/antrian.blade.php
@@ -41,7 +41,7 @@
         <tbody>
             @if ($visits->count() > 0)
                 @foreach ($visits as $visit)
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <tr class="odd:bg-white even:bg-teal-50 border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                     <td class="w-4 p-4">
                         <div class="flex items-center">
                             <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
