12e00f8d90421185842e943b9e47fd90579a9749
Muhammad <sunamuhardi@outlook.com>
2023-04-15 17:01:58 +0700

    harus cek detail pasien sebelum tambahkan data.

--- resources/views/visit/field-create.blade.php
+++ resources/views/visit/field-create.blade.php
@@ -22,14 +22,14 @@
                     Cek Detail Pasien
                 </button>
             </div>
-            <div class="text-center m-5">
+            {{-- <div class="text-center m-5">
                 <x-primary-button class="text-center ml-2">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-1 -ml-1 w-5 h-5">
                         <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z" />
                     </svg>
                     {{ __('Antrian') }}
                 </x-primary-button>
-            </div>
+            </div> --}}
 
             <!-- Main modal -->
             <div id="readPasienModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
